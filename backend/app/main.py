d9ba97dda8fc6bc5c6d7f570db08ad602f56f957
Abuzaid27 <abuzaidpathan4054@gmail.com>
2026-06-08 00:07:35 +0530

    Initial SmartDemand AI release

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://smart-demand-9qbf9u45p-abuzaid-pathan-s-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
